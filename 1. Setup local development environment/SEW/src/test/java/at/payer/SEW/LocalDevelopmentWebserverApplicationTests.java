0b3dc67d29b309c3a6fe382a4dd8b6e901a2d9ea
Felix Payer <1felixpayer1@gmail.com>
2024-09-05 19:14:38 +0200

    Exercise 1 done

---
+++ 1. Setup local development environment/SEW/src/test/java/at/payer/SEW/LocalDevelopmentWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package at.payer.SEW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalDevelopmentWebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
